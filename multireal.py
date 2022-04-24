0e1dc5bc95db3edd0f47fd4b4366453c6ec0993c
AlpTekirdag <72404977+AlpTekirdag@users.noreply.github.com>
2022-04-24 18:23:01 +0300

    graph updated

--- multireal.py
+++ multireal.py
@@ -166,7 +166,7 @@ def main():
     local_max = 0
     total_max =[]
     total_hip = []
-    for i in range(20):
+    for i in range(10):
         total_max.append(0)
         total_hip.append(0)
     count=0
@@ -301,5 +301,28 @@ def main():
 
     # Close the windows.
     cv2.destroyAllWindows()
+
+    time_list = np.array(range(1, 11))
+    first_list=total_max;
+    second_list=total_hip;
+
+    plt.scatter(time_list, first_list, label='angle of arm')
+
+    plt.plot(time_list, first_list, ls="--")
+
+    plt.ylim([0, 180])
+    plt.legend()
+    axes1 = plt.gca()
+    axes2 = axes1.twinx()
+    plt.plot(time_list, second_list, ls="--", c='r')
+    plt.scatter(time_list, second_list, label='angle of back', c='r')
+    axes1.set_ylabel('y-axis 1')
+    axes2.set_ylabel('y-axis 2')
+
+    plt.xlim([0, 11])
+    plt.ylim([100, 260])
+    plt.axhline(y=180, color='r', linestyle='-', label='proper back angle for mt')
+    plt.legend()
+    plt.show()
 
 main()
