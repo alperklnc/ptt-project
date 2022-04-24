78a143306625b9fd8e6a5df7261b61422c3f798f
AlpTekirdag <72404977+AlpTekirdag@users.noreply.github.com>
2022-04-24 19:19:04 +0300

    need fixed

--- multireal.py
+++ multireal.py
@@ -265,6 +265,31 @@ def main():
                 local_max = angle3D
                 total_max[count] = int(local_max)
                 total_hip[count] = hip_angle
+            # graph ploting
+            plt.close("all")
+            time_list = np.array(range(1, 11))
+            first_list = total_max;
+            second_list = total_hip;
+
+            plt.scatter(time_list, first_list, label='angle of arm')
+
+            plt.plot(time_list, first_list, ls="--")
+
+            plt.ylim([0, 180])
+            plt.legend()
+            axes1 = plt.gca()
+            axes2 = axes1.twinx()
+            plt.plot(time_list, second_list, ls="--", c='r')
+            plt.scatter(time_list, second_list, label='angle of back', c='r')
+            axes1.set_ylabel('y-axis 1')
+            axes2.set_ylabel('y-axis 2')
+
+            plt.xlim([0, 11])
+            plt.ylim([100, 260])
+            plt.axhline(y=180, color='r', linestyle='-', label='proper back angle for mt')
+            plt.legend()
+            plt.show(block=False)
+
             if angle3D < 25 and local_max >35:
                 local_max = 0
                 print("hip angle "+str(hip_angle))
@@ -302,27 +327,6 @@ def main():
     # Close the windows.
     cv2.destroyAllWindows()
 
-    time_list = np.array(range(1, 11))
-    first_list=total_max;
-    second_list=total_hip;
 
-    plt.scatter(time_list, first_list, label='angle of arm')
-
-    plt.plot(time_list, first_list, ls="--")
-
-    plt.ylim([0, 180])
-    plt.legend()
-    axes1 = plt.gca()
-    axes2 = axes1.twinx()
-    plt.plot(time_list, second_list, ls="--", c='r')
-    plt.scatter(time_list, second_list, label='angle of back', c='r')
-    axes1.set_ylabel('y-axis 1')
-    axes2.set_ylabel('y-axis 2')
-
-    plt.xlim([0, 11])
-    plt.ylim([100, 260])
-    plt.axhline(y=180, color='r', linestyle='-', label='proper back angle for mt')
-    plt.legend()
-    plt.show()
 
 main()
