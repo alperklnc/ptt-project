00fe6fe51949cebb23737770bcbc41c2896fbce2
Alper Kılınç <akilinc17@ku.edu.tr>
2022-05-09 14:26:03 +0300

    delete example codes

--- front-end/ptt-app/src/App.tsx
+++ front-end/ptt-app/src/App.tsx
@@ -8,20 +8,12 @@ import PatientPage from "./views/patient-page/PatientPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/AddTutorial";
-import Tutorial from "./components/Tutorial";
-import TutorialsList from "./components/TutorialsList";
-
 const App: React.FC = () => {
   return (
     <div>
       <div>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/tutorials" element={<TutorialsList />} />
-          <Route path="/add" element={<AddTutorial />} />
-          <Route path="/tutorials/:id" element={<Tutorial />} />
-
           <Route path="/doctor-page" element={<DoctorPage />} />
           <Route path="/patient-page" element={<PatientPage />} />
           <Route path="/new-patient" element={<NewPatient />} />
