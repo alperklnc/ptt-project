491dbbf1206210796ade0670e0357a2903627457
adarbayan <abayan17@ku.edu.tr>
2022-05-18 19:04:39 +0300

    update

---
+++ flask_image_for_poster.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+picFolder = os.path.join('static', 'pics')
+print(picFolder)
+app.config['UPLOAD_FOLDER'] = picFolder
+
+
+@app.route("/")
+def index():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'sample.jpg')
+    return render_template("index.html", user_image=pic1)   
+
+
+@app.route("/index")
+def index1():
+    return "hello"
+
+
+app.run()
