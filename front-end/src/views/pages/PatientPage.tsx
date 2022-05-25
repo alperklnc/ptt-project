378f7aea690f8ba2c4663a787f614c40e58571b3
Alper Kılınç <akilinc17@ku.edu.tr>
2022-05-25 23:15:32 +0300

    add click feature to patient info component

--- front-end/src/views/pages/PatientPage.tsx
+++ front-end/src/views/pages/PatientPage.tsx
@@ -19,18 +19,8 @@ const PatientPage: React.FC = () => {
   const state = location.state as any;
   const patientId = state.patientId;
 
-  var _sessionId: number = -1;
-  var _sessionData: ISessionData = {
-    id: -1,
-    time: "",
-    date: "",
-    comment: "",
-    pt_id: -1,
-    _completed: false,
-  }
-
   var [sessionData, setSessionData] = useState<ISessionData[]>([]);
-  var [sessionId, setSessionId] = useState(_sessionId);
+  var [sessionId, setSessionId] = useState(-1);
 
   useEffect(() => {
     getSessionId(patientId);
@@ -40,7 +30,6 @@ const PatientPage: React.FC = () => {
     PatientDataService.getSession(patientId)
       .then((response) => {
         setSessionData(response.data);
-        console.log(response.data);
         setSessionId(response.data[0].id);
       })
       .catch((e: Error) => {
@@ -61,34 +50,10 @@ const PatientPage: React.FC = () => {
     .catch((e: Error) => {
       console.log(e);
     });
-
-    /*
-    console.log(sessionId);
-    PatientDataService.getCurrentSession(patientId)
-    .then((response) => {
-      var currentSession = response.data;
-      var newSessionData = sessionData;
-      newSessionData[currentSession]._completed = true;
-      console.log(newSessionData[currentSession]._completed);
-
-      PatientDataService.endSession(patientId, newSessionData)
-      .then((response) => {
-        console.log(response);
-        navigate("/doctor-page");
-      })
-      .catch((e: Error) => {
-        console.log(e);
-      });
-
-    })
-    .catch((e: Error) => {
-      console.log(e);
-    });
-    */
   };
 
   if (sessionId === null || sessionId == -1) {
-    return <h2>Loading posts...</h2>;
+    return <h2>Loading patient page...</h2>;
   }
   return (
     <div>
@@ -119,8 +84,6 @@ const PatientPage: React.FC = () => {
           </Button>
         </Grid>
       </div>
-
-
     </div>
   );
 };
